f3083453b068ff7a9488cfeb901d4ff4c9516591
Naveen-912 <naveenbl2001@gmail.com>
2022-03-18 08:09:43 +0000

    .

--- springapp/src/main/java/com/examly/springapp/registration/controller/RegistrationController.java
+++ springapp/src/main/java/com/examly/springapp/registration/controller/RegistrationController.java
@@ -1,6 +1,26 @@
-package com.examly.springapp.app.registration.registrationcontroller;
+package com.examly.springapp.app.registration.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.examly.springapp.registration.service.RegistrationService;
+import com.examly.springapp.app.registration.model.User;
 @RestController
 public class RegistrationController {
-    
+    @Autowired
+    private RegistrationService service;
+
+    @RequestMapping("/registerUser")
+    public User registerUser(@RequestBody User user){
+        String tempEmailId=user.getEmailId();
+        if(tempEmailId!=null &&!"".equals(tempEmailId)){
+
+        }
+        User userObj=null;
+        userObj =service.saveUser(user);
+        return userObj;
+    }
 }
--- springapp/src/main/java/com/examly/springapp/registration/service/RegistrationService.java
+++ springapp/src/main/java/com/examly/springapp/registration/service/RegistrationService.java
@@ -1,6 +1,14 @@
 package com.examly.springapp.registration.service;
 import org.springframework.stereotype.Service;
+import com.examly.springapp.app.registration.model.User;
+import com.examly.springapp.app.registration.repository.RegistrationRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 @Service
 public class RegistrationService{
+    @Autowired
+    private RegistrationRepository repo;
     
+    public User saveUser(User user){
+        return repo.save(user);
+    }
 }
